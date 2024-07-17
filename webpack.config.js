95bce05a31d6f4a53253de2c673fea2f40bdeec8
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-07-18 02:41:09 +0600

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,4 @@ module.exports = {
     ...defaultConfig.output,
     filename: "[name].js",
   },
-  module: {
-    ...defaultConfig.module,
-    rules: [
-      ...defaultConfig.module.rules,
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-    ],
-  },
 };
