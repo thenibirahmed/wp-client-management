0d65fd551fc623b1553a2131b298a49e88c798ad
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-07-17 13:49:20 +0600

    gitignore updated

--- build/frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'c483ec907d13d499fae7');
--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '32cd159e03859013c4a3');
