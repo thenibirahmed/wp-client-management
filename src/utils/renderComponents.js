304adcc8aabf3883b1b9e528d5c480d348f0329c
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-07-18 03:11:18 +0600

    tab indentation updated

--- src/utils/renderComponents.js
+++ src/utils/renderComponents.js
@@ -8,22 +8,22 @@ import Contact from "../Components/Contact";
 import Settings from "../Components/Settings";
 
 export const renderComponent = (path) => {
-  switch (path) {
-    case "client":
-      return <Clients />;
-    case "project":
-      return <Projects />;
-    case "invoice":
-      return <Invoices />;
-    case "schedule":
-      return <Schedules />;
-    case "report":
-      return <Reports />;
-    case "contact":
-      return <Contact />;
-    case "setting":
-      return <Settings />;
-    default:
-      return <Dashboard />;
-  }
+	switch (path) {
+		case "client":
+			return <Clients />;
+		case "project":
+			return <Projects />;
+		case "invoice":
+			return <Invoices />;
+		case "schedule":
+			return <Schedules />;
+		case "report":
+			return <Reports />;
+		case "contact":
+			return <Contact />;
+		case "setting":
+			return <Settings />;
+		default:
+			return <Dashboard />;
+	}
 };
