e66c176c7fda4ec613497413c3868f561340caca
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-07-18 02:41:32 +0600

    gitignore file rm cached

--- build/frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'd9baa75fec9ae9df2bba');
--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '2ef6dd153380c9dd98ab');
