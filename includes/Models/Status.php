35ebe42c13ee4e5492cee65e4146fad22d8f948a
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-08-12 14:36:45 +0600

    major bug fixed

--- includes/Models/Status.php
+++ includes/Models/Status.php
@@ -2,19 +2,20 @@
 
 namespace WpClientManagement\Models;
 
+use WpClientManagement\Models\Task;
+use WpClientManagement\Models\Project;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Status extends Model
 {
-    protected $table = 'statuses';
+    protected $table = 'eic_statuses';
 
-    
-    public function projects() :HasMany {
+    public function projects() {
         return $this->hasMany(Project::class);
     }
     
-    public function tasks() :HasMany {
+    public function tasks() {
         return $this->hasMany(Task::class);
     }
 
