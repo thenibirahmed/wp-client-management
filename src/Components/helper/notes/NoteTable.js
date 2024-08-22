14c5f21b4131c1628bce43710dc3097679f700e5
EasinTanvir <easintanvir102@gmail.com>
2024-08-22 12:33:48 +0600

    fixed responsive issues

--- src/Components/helper/notes/NoteTable.js
+++ src/Components/helper/notes/NoteTable.js
@@ -59,7 +59,7 @@ const NoteTable = () => {
       <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
           <div className="overflow-hidden shadow ring-1  ring-black ring-opacity-5 sm:rounded-lg">
-            <table className="min-w-full divide-y divide-borderColor ">
+            <table className="xl:min-w-full min-w-[900px] divide-y divide-borderColor ">
               <thead className="bg-gray-50  ">
                 <tr>
                   <th
