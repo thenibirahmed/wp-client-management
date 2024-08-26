7727ba93729210a26e2b881cf7a28725d854dd41
SamSamimz <samsamim595@gmail.com>
2024-08-26 15:43:02 +0600

    added invoice amount for every client

--- includes/API/Clients/ClientOverview.php
+++ includes/API/Clients/ClientOverview.php
@@ -1,15 +1,12 @@
 <?php
-
-namespace WpClientManagement\API\Clients; 
+namespace WpClientManagement\API\Clients;
 
 use WpClientManagement\Models\Client;
 use WpClientManagement\Models\Invoice;
-use WpClientManagement\Models\Project;
 
 class ClientOverview {
 
     private $namespace = 'wp-client-management/v1';
-
     private $endpoint = '/client-overview';
 
     public function __construct()
@@ -25,10 +22,8 @@ class ClientOverview {
 
         $clientsData = Client::getActiveClients();
 
-        // $clientsData->first()->products_count;
-    
         $wp_user_ids = $clientsData->pluck('eic_crm_user.wp_user_id')->toArray();
-        
+
         $wpUsersDb = get_users([
             'include' => $wp_user_ids,
         ]);
@@ -36,48 +31,38 @@ class ClientOverview {
         $wpUsers = [];
         foreach ($wpUsersDb as $user) {
             $wpUsers[$user->ID] = [
-                'name'          => $user->user_login,
-                'email'         => $user->user_email,
-                'display_name'  => $user->display_name,
-                'roles'         => $user->roles,
+                'name'  => $user->user_login,
+                'email' => $user->user_email,
             ];
         }
-        
-        $clientsWithDetails = $clientsData->map(function ($client) use ($wpUsers) {
-            $eicCrmUser = $client->eic_crm_user;
-            $wpUserId = $eicCrmUser->wp_user_id;
-            $wpUser = $wpUsers[$wpUserId] ?? [];
-    
-            return array_merge($client->toArray(), $eicCrmUser->toArray(), $wpUser);
+
+        $clientIds = $clientsData->pluck('id')->toArray();
+        $invoices  = Invoice::whereIn('client_id', $clientIds)->get();
+
+        $invoiceTotals = $invoices->groupBy('client_id')->map(function ($group) {
+            return $group->sum('total');
         });
 
-        $topBar = [
-            [
-                'name' => 'Total Invoice',
-                'amount' => 16400,
-                'subText' => '5 invoices'
-            ],
-            [
-                'name' => 'Total Revenue',
-                'amount' => 12400,
-                'subText' => '3 invoices'
-            ],
-            [
-                'name' => 'Total Due',
-                'amount' => 4000,
-                'subText' => '2 invoices'
-            ],
-            [
-                'name' => 'Total Projects',
-                'amount' => 5,
-                'subText' => 'last 3 months'
-            ]
-        ];
-    
+        $clientsWithDetails = $clientsData->map(function ($client) use ($wpUsers, $invoiceTotals) {
+            $eicCrmUser = $client->eic_crm_user;
+            $wpUserId   = $eicCrmUser->wp_user_id;
+            $wpUser     = $wpUsers[$wpUserId] ?? [];
+
+            $totalInvoiceAmount = $invoiceTotals->get($client->id, 0);
+
+            return [
+                'client_id'             => $client->id,
+                'total_invoice_amount'  => $totalInvoiceAmount,
+                'country'               => $eicCrmUser->country,
+                'organization'          => $client->organization,
+                'project_count'         => $client->projects_count,
+                'name'                  => $wpUser['name'] ?? null,
+                'email'                 => $wpUser['email'] ?? null,
+            ];
+        });
+
         return new \WP_REST_Response([
-            'topBar' => $topBar,
             'clients' => $clientsWithDetails,
         ]);
     }
-    
 }
