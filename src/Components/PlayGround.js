f1ea308a73bf7fb2e955f1dcf9f5284b4eca79f4
SamSamimz <samsamim595@gmail.com>
2024-08-28 19:02:12 +0600

    create email

--- src/Components/PlayGround.js
+++ src/Components/PlayGround.js
@@ -69,14 +69,14 @@ const PlayGround = () => {
 
 	const getTest = () => {
 		axios.post(
-				eicApiSettings.rest_url + "wp-client-management/v1/file/create",
+				eicApiSettings.rest_url + "wp-client-management/v1/email/create",
 				{
 					// title: "Soweb vai title",
 					// manager_id: 1,
 					eic_crm_user_id: 1,
 					client_id: 1,
-					title: "file",
-					url: "https://github.com/SamSamimz.pdf",
+					subject: "no sub",
+					body: "NO BODY", 
 					// status_id: 1,
 					// priority_id: 1,
 					// budget: 50000.00,
