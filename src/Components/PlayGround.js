cdbac15e398bcaa58fede509758bc9f0d0876ba5
SamSamimz <samsamim595@gmail.com>
2024-09-09 12:04:13 +0600

    project note pagination

--- src/Components/PlayGround.js
+++ src/Components/PlayGround.js
@@ -69,7 +69,7 @@ const PlayGround = () => {
 
 	const getTest = () => {
 		axios.get(
-				eicApiSettings.rest_url + "wp-client-management/v1/project/1/tasks?page=3",
+				eicApiSettings.rest_url + "wp-client-management/v1/project/1/notes?note=3",
 				// {
 					// project_id: 1,
 					// date: "2022-09-18",
