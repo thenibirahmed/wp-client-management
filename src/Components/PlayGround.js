fb1238999b05ca6001f8849e4d11b00fba1295bb
SamSamimz <samsamim595@gmail.com>
2024-09-28 12:50:40 +0600

    filter & search in project emails

--- src/Components/PlayGround.js
+++ src/Components/PlayGround.js
@@ -187,7 +187,7 @@ const PlayGround = () => {
 
 	const getTestData = () => {
 		axios.get(
-			eicApiSettings.rest_url + "wp-client-management/v1/project/1/files?from=2010-09-15&to=2024-09-30&search=Samim",
+			eicApiSettings.rest_url + "wp-client-management/v1/project/1/emails?from=2010-09-15&to=2024-09-30&search=subject",
 			{
 				headers: {
 					"X-WP-Nonce": eicApiSettings.nonce,
