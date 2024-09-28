ce95696954497892f721b7d1abb1981a4888f0f3
SamSamimz <samsamim595@gmail.com>
2024-09-28 18:40:20 +0600

    files bulk delete

---
+++ includes/API/Files/FilesBulkDelete.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace WpClientManagement\API\Files;
+
+use WpClientManagement\Models\File;
+
+class FilesBulkDelete {
+
+    private $namespace = 'wp-client-management/v1';
+
+    private $endpoint = '/files/bulk-delete';
+
+    protected array $rules = [
+        'bulk_ids'   => 'nullable|array',
+    ];
+
+    protected array $validationMessages = [
+        'bulk_ids.array'     => 'The bulk IDs must be an array.',
+        'bulk_ids.*.integer' => 'The bulk IDs must be integers.',
+    ];
+
+    public function __construct() {
+        register_rest_route($this->namespace, $this->endpoint, [
+            'methods'  => \WP_REST_Server::DELETABLE,
+            'callback' => array($this, 'bulk_delete_files'),
+            'permission_callback' => 'is_user_logged_in',
+        ]);
+    }
+
+    public function bulk_delete_files(\WP_REST_Request $request) {
+        global $validator;
+
+        $bulk_ids  = $request->get_param('bulk_ids');
+
+        $data = [];
+        $data['bulk_ids'] = isset($bulk_ids) ? $bulk_ids : null;
+
+        $validator = $validator->make($data, $this->rules, $this->validationMessages);
+
+        if ($validator->fails()) {
+            return new \WP_REST_Response([
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        if(empty($bulk_ids) || !is_array($bulk_ids)) {
+            return new \WP_REST_Response([
+                'message' => 'The bulk IDs must be an array.',
+            ], 400);
+        }
+
+        $bulk_delete_files = File::whereIn('id', $bulk_ids)->get();
+
+        foreach ($bulk_delete_files as $file) {
+            $file->delete();
+        }
+
+        return new \WP_REST_Response([
+            'message' => 'Files deleted successfully.',
+        ], 200);
+    }
+}
