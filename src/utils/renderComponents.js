90e7a5aadb1fdf9be1790073d4898bf754c022a1
EasinTanvir <easintanvir102@gmail.com>
2024-10-27 19:39:50 +0600

    added lazy loader

--- src/utils/renderComponents.js
+++ src/utils/renderComponents.js
@@ -1,22 +1,31 @@
-import Dashboard from "../Components/Dashboard/Dashboard";
-import Invoices from "../Components/Invoices";
-import Schedules from "../Components/schedules/Schedules";
-import Contact from "../Components/Contacts/Contact";
-import Settings from "../Components/Settings";
-import Docs from "../Components/Docs";
-import ClientReport from "../Components/ClientReport/ClientReport";
-import Revenue from "../Components/Revenue/Revenue";
-import Expense from "../Components/Expense/Expense";
-import ProfitLoss from "../Components/ProfitLoss/ProfitLoss";
-import Client from "../Components/Clients/Client";
-import Projects from "../Components/Projects/Projects";
+import React, { lazy } from "react";
 
-import ClientDetails from "../Components/Clients/ClientDetails";
-import ProjectDetail from "../Components/Projects/ProjectDetail";
-import ProjectTaskDetails from "../Components/Projects/ProjectTask/ProjectTaskDetails";
-import ContactTeamDetails from "../Components/Contacts/Team/TeamDetails/ContactTeamDetails";
-import Errors from "../Components/Errors";
-import LogIn from "../Components/LogIn/LogIn";
+const Dashboard = lazy(() => import("../Components/Dashboard/Dashboard"));
+const Invoices = lazy(() => import("../Components/Invoices"));
+const Schedules = lazy(() => import("../Components/schedules/Schedules"));
+const Contact = lazy(() => import("../Components/Contacts/Contact"));
+const Settings = lazy(() => import("../Components/Settings"));
+const Docs = lazy(() => import("../Components/Docs"));
+const ClientReport = lazy(() =>
+  import("../Components/ClientReport/ClientReport")
+);
+const Revenue = lazy(() => import("../Components/Revenue/Revenue"));
+const Expense = lazy(() => import("../Components/Expense/Expense"));
+const ProfitLoss = lazy(() => import("../Components/ProfitLoss/ProfitLoss"));
+const Client = lazy(() => import("../Components/Clients/Client"));
+const Projects = lazy(() => import("../Components/Projects/Projects"));
+const ClientDetails = lazy(() => import("../Components/Clients/ClientDetails"));
+const ProjectDetail = lazy(() =>
+  import("../Components/Projects/ProjectDetail")
+);
+const ProjectTaskDetails = lazy(() =>
+  import("../Components/Projects/ProjectTask/ProjectTaskDetails")
+);
+const ContactTeamDetails = lazy(() =>
+  import("../Components/Contacts/Team/TeamDetails/ContactTeamDetails")
+);
+const Errors = lazy(() => import("../Components/Errors"));
+const LogIn = lazy(() => import("../Components/LogIn/LogIn"));
 
 export const renderComponent = (path, activeUrl, paginationUrl) => {
   switch (path) {
