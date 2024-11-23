033b4ad07fdf176f7157fba073daec9e453916ec
EasinTanvir <easintanvir102@gmail.com>
2024-11-24 00:11:12 +0600

    update skeleton

--- src/Components/Skeleton.js
+++ src/Components/Skeleton.js
@@ -4,64 +4,64 @@ const Skeleton = () => {
   return (
     <div role="status" className="space-y-2.5 animate-pulse w-full">
       <div className="flex items-center w-full">
-        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 flex-1" />
+        <div className="h-2.5 bg-gray-100 rounded-full w-32" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full w-24" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full flex-1" />
       </div>
       <div className="flex items-center w-full">
-        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 flex-1" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 flex-1" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24" />
+        <div className="h-2.5 bg-gray-100 rounded-full flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full w-24" />
       </div>
       <div className="flex items-center w-full">
-        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 flex-1" />
-        <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 flex-1" />
+        <div className="h-2.5 bg-gray-200 rounded-full flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-100 rounded-full w-80" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full flex-1" />
       </div>
       <div className="flex items-center w-full">
-        <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 flex-1" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 flex-1" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24" />
+        <div className="h-2.5 ms-2 bg-gray-100 rounded-full flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full w-24" />
       </div>
       <div className="flex items-center w-full">
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-32" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24" />
-        <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full w-32" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full w-24" />
+        <div className="h-2.5 ms-2 bg-gray-100 rounded-full flex-1" />
       </div>
       <div className="flex items-center w-full">
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 flex-1" />
-        <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-100 rounded-full w-80" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full flex-1" />
       </div>
       <div className="flex items-center w-full">
-        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 flex-1" />
+        <div className="h-2.5 bg-gray-100 rounded-full w-32" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full w-24" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full flex-1" />
       </div>
       <div className="flex items-center w-full">
-        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 flex-1" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 flex-1" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24" />
+        <div className="h-2.5 bg-gray-100 rounded-full flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full w-24" />
       </div>
       <div className="flex items-center w-full">
-        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 flex-1" />
-        <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 flex-1" />
+        <div className="h-2.5 bg-gray-200 rounded-full flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-100 rounded-full w-80" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full flex-1" />
       </div>
       <div className="flex items-center w-full">
-        <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 flex-1" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 flex-1" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24" />
+        <div className="h-2.5 ms-2 bg-gray-100 rounded-full flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full w-24" />
       </div>
       <div className="flex items-center w-full">
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-32" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24" />
-        <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full w-32" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full w-24" />
+        <div className="h-2.5 ms-2 bg-gray-100 rounded-full flex-1" />
       </div>
       <div className="flex items-center w-full">
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 flex-1" />
-        <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80" />
-        <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full flex-1" />
+        <div className="h-2.5 ms-2 bg-gray-100 rounded-full w-80" />
+        <div className="h-2.5 ms-2 bg-gray-200 rounded-full flex-1" />
       </div>
     </div>
   );
