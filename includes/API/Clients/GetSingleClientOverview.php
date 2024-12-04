87c758fb939425d11f6e55969edd8d2a00d3d15c
Jamiul Bari <jamiul.bari98@gmail.com>
2024-12-04 23:24:59 +0600

    chore: add designation field in client/id/overview profile

--- includes/API/Clients/GetSingleClientOverview.php
+++ includes/API/Clients/GetSingleClientOverview.php
@@ -78,6 +78,7 @@ class GetSingleClientOverview {
             'phone'        => $clientData->eic_crm_user->phone,
             'address'      => $clientData->eic_crm_user->address,
             'organization' => $clientData->organization,
+            'designation'  => $clientData->eic_crm_user->designation,
         ];
 
         $totalProjects  = Project::where('client_id', $data['id'])->count();
